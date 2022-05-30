5a9cb1c2e9f32454295c5b6edf3417ab659ab454
robertuptc <robertuptc@gmail.com>
2022-05-30 11:47:00 -0400

    finished exercise

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,21 @@
 exports.anagramsFor = function(word, listOfWords) {
+    
+    let answer = []
+    //sorting the word parameter alphabetically
+    sortWord = word.split('').sort().join('')
+
+    //sorting each word in listOfWords alphabetically
+    sortListOfWords = listOfWords.map(elem => elem.split('').sort().join(''))
+
+    //iterating through the sortListOfWords and check if each element == sortWord
+    for (let i = 0; i < sortListOfWords.length; i++) {
+        if (sortListOfWords[i] === sortWord) {
+            answer.push(listOfWords[i])
+        }
+    } return answer
 
 };
+
+
+
+
--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,18 @@
 # Don't forget to run the tests (and create some of your own)
 
+from ast import JoinedStr
+from operator import indexOf
+
+
 def anagrams_for(word, list_of_words):
-		# your code here
+	answer = []
+	sort_word = ("".join((sorted(word))))
+
+
+	for i in range(0, len(list_of_words), 1) :
+		idx_word = ("".join(sorted(list(list_of_words[i]))))
+		if idx_word == sort_word :
+			answer.append(list_of_words[i])
+	
+	return answer
+
